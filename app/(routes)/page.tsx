e6db2f2db32b88594d7952b06725a08507de308f
13PremPatel <113961398+13PremPatel@users.noreply.github.com>
2023-12-31 18:11:20 +0530

    Project

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,27 @@
+import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
+import ProductList from "@/components/product-list";
+import Billboard from "@/components/ui/billboard";
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const products = await getProducts({ isFeatured: true });
+  const billboard = await getBillboard("8c246627-a7f1-4665-9f8e-b2120818457a"); //Billboard ID of Home Page which is not decided via Admin Panel but by putting the Id of thta Billboard here {Will make it Dynamic in the Future}
+
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard 
+          data={billboard}
+        />
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" items={products} />
+        </div>
+      </div>
+    </Container>
+  )
+};
+
+export default HomePage;
